bc83789693bdf43f4913d1091dd75b49290caa10
Vilas Patil <patildipak422@gmail.com>
2020-08-29 02:21:47 +0530

    Modifiation of Minimum and maximum element

--- Maximum_and_Minimum_Of_Array_Elements.c
+++ Maximum_and_Minimum_Of_Array_Elements.c
@@ -1,32 +1,39 @@
 #include<stdio.h>
-int MaximumElement();
-int MinimumElement();
-int main(){
+int MaximumElement(int testarray[], int size);
+int MinimumElement(int testarray[], int size);
+int MaximumElement(int testarray[], int size){
+	int i, max=testarray[0];
+	for(i=0; i<size; i++){
+			if(testarray[i]>max){
+				max=testarray[i];
+			}
+	}
+	return max;
+}
+int MinimumElement(int testarray[], int size){
+	int i, min=testarray[0];
+	for(i=0; i<size; i++){
+			if(testarray[i]<min){
+				min=testarray[i];
+			}
+	}
+	return min;
+}
+void main(){
 	int i, testcases, size, max, min;
-	int testarray[100], duplicatearray[100];
+	int testarray[100];
 	printf("Enter the Testcases :");
 	scanf("%d", &testcases);
 	while(testcases>0){
 		printf("Enter the size :");
 		scanf("%d", &size);
 		for(i=0; i<size; i++){
-			printf("Enter the Element :");
+			printf("Enter the %d Element :", i);
 			scanf("%d", &testarray[i]);
 		}
-		max=testarray[0];
-		min=testarray[0];
-		for(i=0; i<size; i++){
-			if(testarray[i]>max){
-				max=testarray[i];
-			}
-		}
-		for(i=0; i<size; i++){
-			if(testarray[i]<min){
-				min=testarray[i];
-			}
-		}
-		printf("The Maxinum is %d and Minimum is %d\n", max, min);
+		int max=MaximumElement(testarray, size);
+		int min=MinimumElement(testarray, size);
+		printf("The Maximum is %d and Minimum is %d\n", max, min);
 		testcases--;
 	}
-	return 0;
 }
