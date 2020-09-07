497b4c37326007b518f86e801b73e1794d99a04f
Vilas Patil <patildipak422@gmail.com>
2020-09-07 23:10:38 +0530

    initaial Commit

---
+++ longest_string.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main(){
+	int i, testarray1[100], testarray2[100], testarray3[100], testarray4[100], testarray5[100];
+	printf("Enter the length of string : ");
+	scanf("%d", &length);
+	for(i=0; i<length; i++){
+		printf("Enter the letter : ");
+		scanf("%d", &testarray1[i]);
+	}
+	printf("Enter the length of string : ");
+	scanf("%d", &length);
+	for(i=0; i<length; i++){
+		printf("Enter the letter : ");
+		scanf("%d", &testarray2[i]);
+	}
+	printf("Enter the length of string : ");
+	scanf("%d", &length);
+	for(i=0; i<length; i++){
+		printf("Enter the letter : ");
+		scanf("%d", &testarray3[i]);
+	}
+	printf("Enter the length of string : ");
+	scanf("%d", &length);
+	for(i=0; i<length; i++){
+		printf("Enter the letter : ");
+		scanf("%d", &testarray4[i]);
+	}
+	printf("Enter the length of string : ");
+	scanf("%d", &length);
+	for(i=0; i<length; i++){
+		printf("Enter the letter : ");
+		scanf("%d", &testarray5[i]);
+	}
+	return 0;
+}
