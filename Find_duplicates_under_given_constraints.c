7198e970b5539f2d7c167c0249a11b4bcd11c414
Vilas Patil <patildipak422@gmail.com>
2020-09-07 23:10:00 +0530

    final commit

--- Find_duplicates_under_given_constraints.c
+++ Find_duplicates_under_given_constraints.c
@@ -1,15 +1,42 @@
-#inlude<stdio.h>
+#include<stdio.h>
 main(){
-	int testarray[100];
-	int testcases, size;
+	int testarray[100], duplicatearray[100], repeatedelements[100];
+	int testcases, size, index=0, i, j, k;
+	for(i=0; i<=100; i++){
+		repeatedelements[i]='*';
+	}
 	printf("Enter the Testcases :");
 	scanf("%d", &testcases);
 	while(testcases>0){
-		printf("Enter the Size ; ");
+		printf("Enter the Size : ");
 		scanf("%d", &size);
-		for(int i=0; i<size; i++){
+		for(i=0; i<size; i++){
 			printf("Enter the %d Element :", i);
 			scanf("%d", &testarray[i]);
+		}
+		for(i=0; i<size; i++){
+			duplicatearray[i]=testarray[i];
+		}
+		for(i=0; i<size; i++){
+			for(i=0; i< size; i++){
+				if(i==j){
+					continue;
+				}
+				else if(testarray[i]==duplicatearray[j]){
+						repeatedelements[index]=testarray[i];
+				}
+				index++;
+			}
+		}
+/*	for(i=0; i<size; i++){
+			if(repeatedelements[i]==NULL){
+			printf("There are No duplicate")
+		}
+		
+		}*/
+		printf("The Duplicate Elements are :");
+		for(i=0; i<size; i++){
+			printf("%d ", repeatedelements[i]);
 		}
 	}
 	return 0;
