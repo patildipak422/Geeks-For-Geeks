b3ea1a43f5afe560a79adcaa9e8c7ff7b5327476
Vilas Patil <patildipak422@gmail.com>
2020-09-07 23:19:07 +0530

    final commit

--- longest_string.c
+++ longest_string.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
+void enterstring();
 int main(){
-	int i, testarray1[100], testarray2[100], testarray3[100], testarray4[100], testarray5[100];
+	int i, testarray1[100];
+	enterstring();
+	for(i=0; i<10; i++){
+		printf("%d\n", testarray1[i]);
+	}
+
+}
+void enterstring(){
+	int i, length, testarray1[100], testarray2[100], testarray3[100], testarray4[100], testarray5[100];
 	printf("Enter the length of string : ");
 	scanf("%d", &length);
 	for(i=0; i<length; i++){
@@ -11,22 +20,22 @@ int main(){
 	scanf("%d", &length);
 	for(i=0; i<length; i++){
 		printf("Enter the letter : ");
-		scanf("%d", &testarray2[i]);
+		scanf("%c", &testarray2[i]);
 	}
 	printf("Enter the length of string : ");
 	scanf("%d", &length);
 	for(i=0; i<length; i++){
 		printf("Enter the letter : ");
-		scanf("%d", &testarray3[i]);
+		scanf("%c", &testarray3[i]);
 	}
 	printf("Enter the length of string : ");
-	scanf("%d", &length);
+	scanf("%c", &length);
 	for(i=0; i<length; i++){
 		printf("Enter the letter : ");
-		scanf("%d", &testarray4[i]);
+		scanf("%c", &testarray4[i]);
 	}
 	printf("Enter the length of string : ");
-	scanf("%d", &length);
+	scanf("%c", &length);
 	for(i=0; i<length; i++){
 		printf("Enter the letter : ");
 		scanf("%d", &testarray5[i]);
