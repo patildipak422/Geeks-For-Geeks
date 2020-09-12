e0e4edc0c79265da58a05798c06890c0cd5d2a7a
Vilas Patil <patildipak422@gmail.com>
2020-09-12 14:33:30 +0530

    Multiplying all the Array Elements using function

--- multiply_array.c
+++ multiply_array.c
@@ -1,19 +1,24 @@
 #include<stdio.h>
+void multiply(int testcases);
 int main(){
-	int testcases, i, size, multiplication, testarray[100];
-//	printf("Enter the number of testcases : ");
+	int testcases;
+	printf("Enter the number of testcases : ");
 	scanf("%d", &testcases);
-	while(testcases!=0){
-		multiplication=1;
-		//printf("Enter the Number of Elements : ");
-		scanf("%d", &size);
-		for(i=0; i<size; i++){
-		//	printf("Enter the Element : ");
-			scanf("%d", &testarray[i]);
-			multiplication=multiplication*testarray[i];
-		}
-		printf("%d\n", multiplication);
-		testcases--;
-	}
+	multiply(testcases);
 	return 0;
+}
+void multiply(int testcases){
+	int i, size, multiplication, testarray[100];
+	while(testcases!=0){
+	multiplication=1;
+	printf("Enter the Number of Elements : ");
+	scanf("%d", &size);
+	for(i=0; i<size; i++){
+		printf("Enter the Element : ");
+		scanf("%d", &testarray[i]);
+		multiplication=multiplication*testarray[i];
+	}
+	printf("%d\n", multiplication);
+	testcases--;
+	}
 }
