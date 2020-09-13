e113ff62abe99dacc5032a2d13cceb375001b869
Vilas Patil <patildipak422@gmail.com>
2020-09-13 22:08:06 +0530

    PRint Alternate Elements using Function call

--- Print_alternate_elements_of_an_array.c
+++ Print_alternate_elements_of_an_array.c
@@ -1,22 +1,26 @@
 #include<stdio.h>
 void main(){
-	int testarray[100];
-	int size, testcases, i;
+	int testcases;
 	printf("Enter the testcases :");
 	scanf("%d", &testcases);
-	while(t>0){
-		printf("Enter the size :");
-		scanf("%d", &size);
-		for(i=0; i<size; i++){
-			printf("Enter the Element :");
-			scanf("%d", &testarray[i]);
-		}
-		printf("The Alternative Elements in the array are :");
-		for(i=0; i<size;){
-			printf("%d ", testarray[i]);
-			i+=2;
-		}
-		printf("\n");
-	t--;
+	while(testcases>0){
+	code();
+	testcases--;
 	}
+}
+int code(){
+	int testarray[100];
+	int i, size;
+	printf("Enter the size :");
+	scanf("%d", &size);
+	for(i=0; i<size; i++){
+		printf("Enter the Element :");
+		scanf("%d", &testarray[i]);
+	}
+	printf("The Alternative Elements in the array are :");
+	for(i=0; i<size;){
+		printf("%d ", testarray[i]);
+		i+=2;
+	}
+	printf("\n");
 }
